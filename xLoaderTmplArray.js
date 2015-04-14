7e3f9243a7e2328ce8c924dc977e4b99ed718f7f
Anas Tawfeek <anas.tawfeek@gmail.com>
2015-04-14 12:55:52 +0200

    Adding xLoader files

---
+++ xLoaderTmplArray.js
@@ -0,0 +1,15 @@
+var xLoaderTmpl = {
+	AudioWave: '<div id="xLoader"><div class="audio-wave"><span></span><span></span><span></span><span></span><span></span></div></div>',
+
+	CircularSquare: '<div id="xLoader"><div class="circular-square"><span></span><span></span><span></span><span></span></div></div>',
+
+	CrossingShapes: '<div id="xLoader"><div class="crossing-shapes"></div></div>',
+
+	GlisteningWindow: '<div id="xLoader"><div class="glistening-window"><span></span><span></span><span></span><span></span></div></div>',
+
+	GoogleSpin: '<div id="xLoader"><div class="google-spin-wrapper"><div class="google-spin"></div></div></div>',
+
+	SpinningDisc: '<div id="xLoader"><div class="spinning-disc"></div></div>',
+
+	TheSnake: '<div id="xLoader"><div class="the-snake"><span></span><span></span><span></span><span></span><span></span></div></div>'
+}
